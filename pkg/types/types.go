0bd72ea3da56e4fcf3a95e181a065d527100a15a
Jamoliddin Kodirov <princeofmath13@gmail.com>
2021-09-20 08:48:52 +0500

    types added

---
+++ pkg/types/types.go
@@ -0,0 +1,38 @@
+package types
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type PAN string
+
+type Category string
+
+type Card struct {
+	ID int
+	PAN PAN
+	Balance Money
+	MinBalance Money
+	Currency Currency
+	Color string
+	Name string
+	Active bool
+}
+
+type Payment struct {
+	ID int
+	Amount Money
+	Category Category
+}
+
+type PaymentSource struct {
+	Type string
+	Number string
+	Balance Money
+}
